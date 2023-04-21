57c95440a6812ace00395086f30bfb010884009d
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-21 18:35:42 +0530

    Refactored summary, implemented independent updateScore function

--- src/cricketSummary.js
+++ src/cricketSummary.js
@@ -1,21 +1,33 @@
+const updateScore = function(tally, delivery) {
+  if(tally[delivery] !== undefined && typeof(delivery) !== "string") {
+    tally[delivery] += 1;
+  }
+
+  if(typeof(delivery) !== "string") {
+    tally.total += delivery;
+  }
+
+  if(delivery === "NB" || delivery === "WD") {
+    tally[delivery] += 1;
+    tally.total += 1;
+  }
+
+  if(delivery === "W") {
+    tally[delivery] += 1;
+  }
+
+  tally.extras = tally.WD + tally.NB;
+
+  return tally;
+};
+
 const summary = function(overs) { 
   const innings = overs.flatMap(function(over) {return over;})
+  const tally = {total: 0, 4: 0, 6: 0, W: 0, WD: 0, NB: 0, extras: 0};
 
-  return innings.reduce(function(tally, ball){
-
-    if(tally[ball] !== undefined) {
-      tally[ball] += 1;
-    }
-
-    if(ball !== "NB" && ball !== "WD" && ball !== "W") {
-      tally.total += ball;
-    }
-
-    tally.extras = tally.WD + tally.NB;
-    tally.total += tally.extras;
-
-    return tally;
-  },{total: 0, 4: 0, 6: 0, W: 0, WD: 0, NB: 0, extras: 0})
+  return innings.reduce(updateScore, tally); 
 };
+
+console.log(summary([[4, 6]]));
 
 exports.summary = summary;
